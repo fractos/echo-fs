3c3d0a0f8d4fb9c3263ec93a9c59858f98e75012
fractos <fractos@gmail.com>
2016-06-09 12:14:13 +0100

    correct name for third parameter.

--- echo_listener.sh
+++ echo_listener.sh
@@ -5,6 +5,6 @@ cd /opt/echo-fs
 echo "Activating python virtual environment"
 source venv/bin/activate
 
-echo "Starting echo-listener with Redis=$1, Region=$2, AZ=$3"
+echo "Starting echo-listener with Redis=$1, Region=$2, Queue=$3"
 
 python echo_listener.py $1 $2 $3
