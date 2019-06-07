daffe143eab7d7dc70d71a2c6b5480e208e04d13
Adam Christie <fractos@gmail.com>
2019-06-07 12:35:10 +0100

    remove shell scripts.

--- app/echo_listener.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /opt/echo-fs
-
-./wait_for_redis.sh
-
-echo "Starting echo-listener with Redis=$ECHO_REDIS_HOST, RedisPort=$ECHO_REDIS_PORT, RedisDB=$ECHO_REDIS_DB, Region=$ECHO_QUEUE_REGION, InputQueue=$ECHO_INPUT_QUEUE, ErrorQueue=$ECHO_ERROR_QUEUE"
-
-python -u echo_listener.py
--- app/echo_scavenger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /opt/echo-fs
-
-./wait_for_redis.sh
-
-echo "Starting echo-scavenger with RedisHost=$ECHO_REDIS_HOST, RedisPort=$ECHO_REDIS_PORT, RedisDB=$ECHO_REDIS_DB"
-
-python -u echo_scavenger.py
